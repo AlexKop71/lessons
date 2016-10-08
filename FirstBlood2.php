2376e1efe917392559808a61ba5a626bc2c83aaa
AlexKop71 <leshakop71@yandex.ru>
2016-10-08 14:59:41 +0400

    Первый коммит!

---
+++ FirstBlood2.php
@@ -0,0 +1,29 @@
+<?php
+
+function Welcome (){
+    $name = "Алексей" ;
+    $surname = "Коптяев" ;
+    echo "Здравствуй". "$name". "$surname". "!";
+}
+Welcome() ;
+
+function CheckAge ($age){
+    if($age>18 and $age<59){
+        echo "Вам ещё работать и работать";
+    }
+    if($age>59){
+        echo "Вам пара на пенсию";
+    }
+    if($age<18){
+        echo "Вам ещё рано работать";
+    }
+}
+CheckAge(60);
+
+
+for ($i=0 ; $i <= 100 ; $i++ );{
+if ($i%2==0){
+echo "br/";
+}
+}
+
---
+++ первая кровь.php
@@ -0,0 +1,21 @@
+<?php
+
+function Welcome (){
+    $name = "Алексей" ;
+    $surname = "Коптяев" ;
+    echo "Здравствуй $name $surname!";
+}
+Welcome() ;
+
+function CheckAge ($age){
+    if($age= 18 and $age<18) {
+        echo "Школа";
+    }
+if ($age=3 and $age <7) {
+    echo "Садик";
+}
+if($age=18 and $age <21) {
+    echo "Университет";
+}
+}
+CheckAge(20);
