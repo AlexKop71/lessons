6b918c83cf6723afdab69c3bb61c1b304a2d0d5b
AlexKop71 <leshakop71@yandex.ru>
2016-10-14 04:47:55 +0400

    Второй коммит

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php
 
-$year['Winter']['Monthes']['1'] ='Декабрь';
-$year['Winter']['Monthes']['2']='Январь';
-$year['Winter']['Monthes']['3']='Февраль';
-echo $year['Winter']['Monthes']['1']."<br />";
-echo $year['Winter']['Monthes']['2']."<br />";
-echo $year['Winter']['Monthes']['3']."<br />";
+    $year['Winter']['Monthes']['1'] ='Декабрь';
+    $year['Winter']['Monthes']['2']='Январь';
+    $year['Winter']['Monthes']['3']='Февраль';
+    echo $year['Winter']['Monthes']['1']."<br />";
+    echo $year['Winter']['Monthes']['2']."<br />";
+    echo $year['Winter']['Monthes']['3']."<br />";
 
-$year['Spring']['Monthes']['1'] ='Март';
-$year['Spring']['Monthes']['2']='Апрель';
-$year['Spring']['Monthes']['3']='Май';
-echo $year['Spring']['Monthes']['1']."<br />";
-echo $year['Spring']['Monthes']['2']."<br />";
-echo $year['Spring']['Monthes']['3']. "<br />";
+    $year['Spring']['Monthes']['1'] ='Март';
+    $year['Spring']['Monthes']['2']='Апрель';
+    $year['Spring']['Monthes']['3']='Май';
+    echo $year['Spring']['Monthes']['1']."<br />";
+    echo $year['Spring']['Monthes']['2']."<br />";
+    echo $year['Spring']['Monthes']['3']. "<br />";
 
-$year['Summer']['Monthes']['1'] ='Июнь';
-$year['Summer']['Monthes']['2']='Июль';
-$year['Summer']['Monthes']['3']='Август';
-echo $year['Summer']['Monthes']['1']."<br />";
-echo $year['Summer']['Monthes']['2']."<br />";
-echo $year['Summer']['Monthes']['3']. "<br />";
+    $year['Summer']['Monthes']['1'] ='Июнь';
+    $year['Summer']['Monthes']['2']='Июль';
+    $year['Summer']['Monthes']['3']='Август';
+    echo $year['Summer']['Monthes']['1']."<br />";
+    echo $year['Summer']['Monthes']['2']."<br />";
+    echo $year['Summer']['Monthes']['3']. "<br />";
 
-$year['Outumm']['Monthes']['1']='Сентябрь';
-$year['Outumm']['Monthes']['2']='Октябрь';
-$year['Outumm']['Monthes']['3']='Ноябрь';
-echo $year['Outumm']['Monthes']['1']."<br />";
-echo $year['Outumm']['Monthes']['2']."<br />";
-echo $year['Outumm']['Monthes']['3']. "<br />";
+    $year['Outumm']['Monthes']['1']='Сентябрь';
+    $year['Outumm']['Monthes']['2']='Октябрь';
+    $year['Outumm']['Monthes']['3']='Ноябрь';
+    echo $year['Outumm']['Monthes']['1']."<br />";
+    echo $year['Outumm']['Monthes']['2']."<br />";
+    echo $year['Outumm']['Monthes']['3']. "<br />";
 
